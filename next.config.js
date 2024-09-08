64233a42546591f0eea658a9d45068d6455de257
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2024-09-08 23:41:35 +0330

    remove All

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['cataas.com'], // اضافه کردن دامنه مجاز برای تصاویر
-    },
-}
-
-module.exports = nextConfig
